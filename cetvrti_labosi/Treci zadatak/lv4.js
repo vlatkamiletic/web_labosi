df8ad1765336d7757cc4f82c8c007fedb813d824
vlatkamiletic <vlatkamiletic03@gmail.com>
2024-11-15 01:03:33 +0100

    Cetvrti labosi

---
+++ cetvrti_labosi/Treci zadatak/lv4.js
@@ -0,0 +1,11 @@
+class Macka{
+    constructor(ime, boja, dob, spol){
+        this.ime = ime;
+        this.boja = boja;
+        this.dob = dob;
+        this.spol = spol;
+    }
+    setDob(dob){
+        this.dob = dob;
+    }
+}
